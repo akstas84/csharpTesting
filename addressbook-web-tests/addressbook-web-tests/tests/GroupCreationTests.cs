4baf40a0b683056d4b0d64237ca568c6eeb024d9
akstas84 <akstas84@gmail.com>
2018-09-05 13:22:48 +0300

    Revert "commit"
    This reverts commit dbf410cc70f134184d690eb3c425a457378a89db.

--- addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -13,6 +13,7 @@ namespace WebAddressbookTests
     [TestFixture]
     public class GroupCreationTests : AuthTestBase
     {
+
         //public static IEnumerable<GroupData> RandomGroupDataProvider()
         //{
         //    List<GroupData> goups = new List<GroupData>();
