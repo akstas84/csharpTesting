7f43c5298c1cea5fcb05a1960da6bb3574f48648
akstas84 <akstas84@gmail.com>
2018-09-05 13:27:39 +0300

    commitdbhh

--- addressbook-web-tests/addressbook-web-tests/tests/GroupRemovalTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupRemovalTests.cs
@@ -11,13 +11,15 @@ namespace WebAddressbookTests
         [Test]
         public void GroupRemovalTest()
         {
-            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            //List<GroupData> oldGroups = app.Groups.GetGroupList();
+            List<GroupData> oldGroups = GroupData.GetAll();
 
             app.Groups.Remove(0);
 
             Assert.AreEqual(oldGroups.Count -1, app.Groups.GetGroupCount());
 
-            List<GroupData> newGroups = app.Groups.GetGroupList();
+            //List<GroupData> newGroups = app.Groups.GetGroupList();
+            List<GroupData> newGroups = GroupData.GetAll();
 
             oldGroups.RemoveAt(0);
             Assert.AreEqual(oldGroups, newGroups);
